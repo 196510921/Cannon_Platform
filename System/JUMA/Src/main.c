2cecdf5551598aab026027112b8a436ae27900a2
Junfeng Geng <junfeng_geng@hotmail.com>
2016-01-07 16:36:32 +0800

    fix spacing error

--- System/JUMA/Src/main.c
+++ System/JUMA/Src/main.c
@@ -1,42 +1,42 @@
 /**
-  ******************************************************************************
-  * @file    main.c 
-  * @author  CL
-  * @version V1.0.0
-  * @date    04-July-2014
-  * @brief   This application contains an example which shows how implementing
-  *          a proprietary Bluetooth Low Energy profile: the sensor profile.
-  *          The communication is done using a Nucleo board and a Smartphone
-  *          with BTLE.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+******************************************************************************
+* @file    main.c 
+* @author  CL
+* @version V1.0.0
+* @date    04-July-2014
+* @brief   This application contains an example which shows how implementing
+*          a proprietary Bluetooth Low Energy profile: the sensor profile.
+*          The communication is done using a Nucleo board and a Smartphone
+*          with BTLE.
+******************************************************************************
+* @attention
+*
+* <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*   1. Redistributions of source code must retain the above copyright notice,
+*      this list of conditions and the following disclaimer.
+*   2. Redistributions in binary form must reproduce the above copyright notice,
+*      this list of conditions and the following disclaimer in the documentation
+*      and/or other materials provided with the distribution.
+*   3. Neither the name of STMicroelectronics nor the names of its contributors
+*      may be used to endorse or promote products derived from this software
+*      without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+******************************************************************************
+*/
 
 /* Includes ------------------------------------------------------------------*/
  
@@ -128,93 +128,93 @@ uint32_t stime ;
 
 int main(void)
 {
-	/* STM32Cube HAL library initialization:
-   *  - Configure the Flash prefetch, Flash preread and Buffer caches
-   *  - Systick timer is configured by default as source of time base, but user 
-   *    can eventually implement his proper time base source (a general purpose 
-   *    timer for example or other time source), keeping in mind that Time base 
-   *    duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
-   *    handled in milliseconds basis.
-   *  - Low Level Initialization
-   */
-	
-  HAL_Init();
-  /* Configure the system clock */
-  SystemClock_Config();
-	/* Configure LED0 */
-  BSP_LED_Init(LED0);
-	/*UART2 init*/
-	UART_Init();
-	HAL_Delay(1000);
-	/*sensor layer init*/
-	jsensor_sys_init();
-	/* Initialize the BlueNRG SPI driver */
-  BNRG_SPI_Init();
-  /* Initialize the BlueNRG HCI */
-  HCI_Init();
-  /* Reset BlueNRG hardware */
-  BlueNRG_RST();
-	
-	 /* Enable Power Clock */
-  __HAL_RCC_PWR_CLK_ENABLE();
-	
-	rtc_init();
-	dispatch_init();
-	//send_acc_data(NULL);
-	on_ready();
-  while(1)
-  {
-    HCI_Process();
-		
-		if(Ble_conn_state){
-			Ble_conn_state = BLE_NOCONNECTABLE;
-		}
-		dispatch();
-		if(sleep_flag == 1){
-			StopMode_Measure();
-		}
+    /* STM32Cube HAL library initialization:
+     *  - Configure the Flash prefetch, Flash preread and Buffer caches
+     *  - Systick timer is configured by default as source of time base, but user 
+     *    can eventually implement his proper time base source (a general purpose 
+     *    timer for example or other time source), keeping in mind that Time base 
+     *    duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+     *    handled in milliseconds basis.
+     *  - Low Level Initialization
+     */
+    
+    HAL_Init();
+    /* Configure the system clock */
+    SystemClock_Config();
+    /* Configure LED0 */
+    BSP_LED_Init(LED0);
+    /*UART2 init*/
+    UART_Init();
+    HAL_Delay(1000);
+    /*sensor layer init*/
+    jsensor_sys_init();
+    /* Initialize the BlueNRG SPI driver */
+    BNRG_SPI_Init();
+    /* Initialize the BlueNRG HCI */
+    HCI_Init();
+    /* Reset BlueNRG hardware */
+    BlueNRG_RST();
+    
+    /* Enable Power Clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    
+    rtc_init();
+    dispatch_init();
+    //send_acc_data(NULL);
+    on_ready();
+    while(1)
+    {
+        HCI_Process();
+        
+        if(Ble_conn_state){
+            Ble_conn_state = BLE_NOCONNECTABLE;
+        }
+        dispatch();
+        if(sleep_flag == 1){
+            StopMode_Measure();
+        }
 
-  }
+    }
 }
 
 static void send_acc_data(void * args)
 {
-	
-	BSP_LED_Off(LED0);
-	run_after_delay(send_test_data, NULL, 10000);
+    
+    BSP_LED_Off(LED0);
+    run_after_delay(send_test_data, NULL, 10000);
 }
 
 static void send_test_data(void * args)
 {
-	BSP_LED_On(LED0);
-	run_after_delay(send_acc_data, NULL, 10000);
+    BSP_LED_On(LED0);
+    run_after_delay(send_acc_data, NULL, 10000);
 }
 
 void HAL_RTCEx_WakeUpTimerEventCallback(RTC_HandleTypeDef *hrtc)
 {
-	uwCounter++;
+    uwCounter++;
 }
 
 void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc)
 {
-  /* Turn LED0 on: Alarm generation */
-  BSP_LED_On(LED0);
+    /* Turn LED0 on: Alarm generation */
+    BSP_LED_On(LED0);
 }
 
 
 void sleep_flag_set(uint8_t flag)
 {
-	sleep_flag = flag;
+    sleep_flag = flag;
 }
 
 void Error_Handler(void)
 {
-  while (1)
-  {
-    /* Toggle LED2 with a period of one second */
-    BSP_LED_Toggle(LED0);
-    HAL_Delay(1000);
-  }
+    while (1)
+    {
+        /* Toggle LED2 with a period of one second */
+        BSP_LED_Toggle(LED0);
+        HAL_Delay(1000);
+    }
 }
 
 
